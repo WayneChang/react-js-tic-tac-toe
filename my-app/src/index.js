dfd38cd7ede9098bb6d4a681de827cf60e5ecc24
WayneChang <mail.chewei@gmail.com>
2019-06-02 17:52:07 +0800

    feat: add col row label

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -23,18 +23,26 @@ class Board extends React.Component {
   render() {
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
+          <label className="col-row-category">row\col</label>
+          <label className="col-title">1</label>
+          <label className="col-title">2</label>
+          <label className="col-title">3</label>
+        </div>
+        <div className="board-row">
+          <label className="row-title">1</label>
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
         </div>
         <div className="board-row">
+          <label className="row-title">2</label>
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
         </div>
         <div className="board-row">
+          <label className="row-title">3</label>
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
@@ -111,7 +119,7 @@ class Game extends React.Component {
           />
         </div>
         <div className="game-info">
-          <div>{status}</div>
+          <div className="status">{status}</div>
           <ol>{moves}</ol>
         </div>
       </div>
