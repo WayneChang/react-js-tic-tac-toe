0516cd4643a8efb86546211b6625ef8434074cbf
WayneChang <mail.chewei@gmail.com>
2019-06-04 00:46:43 +0800

    refactor(board, square): extract board, square from index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,56 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Board from './components/board';
 import './index.css';
-
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          <label className="col-row-category">row\col</label>
-          <label className="col-title">1</label>
-          <label className="col-title">2</label>
-          <label className="col-title">3</label>
-        </div>
-        <div className="board-row">
-          <label className="row-title">1</label>
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          <label className="row-title">2</label>
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          <label className="row-title">3</label>
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
 
 class Game extends React.Component {
   constructor(props) {
