7ff5b1f84ec97d0c1230c8c68e635d2277be8516
WayneChang <mail.chewei@gmail.com>
2019-08-16 19:16:48 +0800

    Updates

---
+++ precache-manifest.7442a0a1d8acbdadc0ff6a86e9f1fe00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc58d20cee1c471dc9eb630efd93a8f",
+    "url": "/react-js-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "b23297a51454e77dfe36",
+    "url": "/react-js-tic-tac-toe/static/css/main.66ed466b.chunk.css"
+  },
+  {
+    "revision": "c172514a070990f51700",
+    "url": "/react-js-tic-tac-toe/static/js/2.cb994eee.chunk.js"
+  },
+  {
+    "revision": "b23297a51454e77dfe36",
+    "url": "/react-js-tic-tac-toe/static/js/main.5ebacc3c.chunk.js"
+  },
+  {
+    "revision": "4e5f4b8cd2dcf73777b4",
+    "url": "/react-js-tic-tac-toe/static/js/runtime~main.eeb82545.js"
+  }
+]);
