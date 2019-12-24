df8f2802d0799e4b851ef4abb2b9b391cd13fc01
WayneChang <mail.chewei@gmail.com>
2019-12-25 03:16:29 +0800

    refactor: use grid on board and refactor code

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -24,22 +24,20 @@ class Board extends React.Component {
     const rows = 3;
     const board = [];
     for (let row = 0; row <= rows; row += 1) {
-      const rowComps = [];
       for (let col = 0; col <= cols; col += 1) {
         if (row === 0 && col === 0) {
-          rowComps.push(<label key="row\col" className="col-row-category">row\col</label>);
+          board.push(<label key="row\col" className="board-category">row\col</label>);
         } else if (row === 0) {
-          rowComps.push(<label key={col} className="col-title">{col}</label>);
+          board.push(<label key={`col${col}`} className="board-col-title">{col}</label>);
         } else if (col === 0) {
-          rowComps.push(<label key={row} className="row-title">{row}</label>);
+          board.push(<label key={`row${row}`} className="board-row-title">{row}</label>);
         } else {
-          rowComps.push(this.renderSquare((row - 1) * 3 + col - 1));
+          board.push(this.renderSquare((row - 1) * 3 + col - 1));
         }
       }
-      board.push(<div key={row} className="board-row">{rowComps}</div>);
     }
     return (
-      <div>{board}</div>
+      <div className="board">{board}</div>
     );
   }
 }
